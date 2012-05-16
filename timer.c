ef4fd7917dfa95e19e4bc9a33dddaf0ff81548aa
Per Karlstedt <per.karlstedt@gmail.com>
2012-05-16 23:01:27 +0200

    Change from timer2 to timer0

--- timer.c
+++ timer.c
@@ -9,28 +9,24 @@ volatile uint16_t sys_time_sec_low = 0;
 
 char time[SYSTIME_STR_LEN];
 
-ISR(TIMER2_OVF_vect)
+ISR(TIMER0_OVF_vect)
 {
     TCNT2 = 5;
     sys_time++;
     if (sys_time == 1000)
     {
         sys_time_sec_low++;
-//      if (sys_time_sec_low == 255)
-//      {
-//          sys_time_sec_high++;
-//      }
         sys_time = 0;
     }
 }
 
 void timer_init(void)
 {
-    TCNT2 = 223;
-    TIMSK2 = (1<<TOIE2);
-    TCCR2B = (1<<CS20) | (1<<CS21);
-    //ASSR = (1<<AS2);
-    kprint("System timer initilized with 1ms resolution\n");
+    TCNT0 = 223;
+    TIMSK = (1<<TOIE0);
+    TCCR0 = (1<<CS20) | (1<<CS21);
+    //ASSR = (1<<AS0);
+    printk("System timer initilized with 1ms resolution\n");
 }
 
 static uint16_t get_sys_time_ms(void)
