75e4cfa95d913fdb419d3115baac8ce17c1a16f1
Per Karlstedt <per.karlstedt@gmail.com>
2013-04-27 20:49:44 +0200

    Updates in main program

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #include "ipc.h"
 #include "hw_channel.h"
 #include "reset.h"
-
+#include "list.h"
     ow_device_t *ow_devices;
 uint8_t cmp_pattern(uint8_t *p, uint8_t pattern, size_t size)
 {
@@ -41,19 +41,32 @@ ISR(INT5_vect) //IRQ from aaps_a
 
 //static struct system_settings sys_settings;
 
-int test_cb1(void)
+int blink_led2(void)
 {
-    printk("1\n");
+    printk("LED2 blink\n");
     return 0;
 }
-int test_cb2(void)
+
+int sleep_mode(void)
 {
-    ow_temp_t temp;
-    if (get_scratch_pad_async(&(ow_devices[0]), &temp) == OW_RET_OK)
-            printk("Ambient temperature: %u.%u°C\n",temp.temp, temp.dec);
+    printk("No activity for 20 seconds - Entering sleep mode!\n");
+    return 0;
+}
+
+int trigger_conv_t(void)
+{
     ow_convert_temp_async(&(ow_devices[0]));
     return 0;
 }
+
+int get_temp(void)
+{
+    ow_temp_t temp;
+    if (get_scratch_pad_async(&(ow_devices[0]), &temp) == OW_RET_OK)
+            printk("Temp: %u.%u°C\n",temp.temp, temp.dec);
+    return 0;
+}
+
 int main(void)
 {
     rst_save_reason();
@@ -79,6 +92,10 @@ int main(void)
     cmd_init();
     timer_init();
     ds3234_init();
+    struct list_node_t *list_head = list_init();
+    if (list_head == NULL)
+        printk("Failed to init list\n");
+
     uint8_t ctrl_reg = ds3234_read_ctrl_reg();
 
     ds3234_get_time(&time);
@@ -96,11 +113,11 @@ int main(void)
     uint8_t ow_num_sensors = ow_num_devices();
     //settings_get_settings(&sys_settings);
 
+#define DBG(x) x
     printk("reg: 0x%x\n", ctrl_reg);
     ds3234_write_reg(0x8e, 0x00);
-    printk("reg: 0x%x\n", ds3234_read_ctrl_reg());
+    DBG(printk("reg: 0x%x\n", ds3234_read_ctrl_reg());)
     ret = ipc_periph_detect(&m48, &periph_type);
-
     if (ret != IPC_RET_OK) {
         printk("Error: 0x%x\n", ret);
     } else {
@@ -127,31 +144,26 @@ int main(void)
     //        printk("CRC failed\n");
     //    }
     //}
+
     timer1_init();
     temp.temp = 0;
     temp.dec = 0;
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 95);
-    ow_convert_temp_async(&(ow_devices[0]));
-    timer1_create_timer(test_cb2, 1750, PERIODIC);
 
-    //timer1_create_timer(test_cb2, 7000);
-    //timer1_create_timer(test_cb2, 7000);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb1, 1);
-    //timer1_create_timer(test_cb2, 5); //Should not work since timer is already registered
+    ow_convert_temp_async(&(ow_devices[0]));
+    timer1_create_timer(trigger_conv_t, 30000, PERIODIC, 0);
+    timer1_create_timer(get_temp, 30000, PERIODIC, 200);
+    timer1_create_timer(blink_led2, 1, ONE_SHOT, 30000);
+    timer1_create_timer(sleep_mode, 20000, PERIODIC, 0);
+    //timer1_create_timer(test_cb2, 5, ONE_SHOT, 0); //Should not work since timer is already registered
+
     //int fan_speed = 0;
     uint8_t cnt = 0;
     while(1)
     {
         printk("Entering main loop\n");
         while(1) {
-        pending_cmd();
-        wdt_reset();
+            pending_cmd();
+            wdt_reset();
         }
     //ctrl_reg = ds3234_read_ctrl_reg();
     //printk("reg: 0x%x\n", ctrl_reg);
